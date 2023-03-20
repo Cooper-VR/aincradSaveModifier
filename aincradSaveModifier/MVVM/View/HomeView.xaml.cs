d74a7161f93f946ba21b32948486950456d8585f
-(Cooper)- <103378143+Cooper-VR@users.noreply.github.com>
2023-03-20 14:32:22 -0700

    FIX FONT ADDING

--- aincradSaveModifier/MVVM/View/HomeView.xaml.cs
+++ aincradSaveModifier/MVVM/View/HomeView.xaml.cs
@@ -1,4 +1,17 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace aincradSaveModifier.MVVM.View
 {
@@ -9,7 +22,7 @@ namespace aincradSaveModifier.MVVM.View
     {
         public HomeView()
         {
-            InitializeComponent();
+            
         }
     }
 }
